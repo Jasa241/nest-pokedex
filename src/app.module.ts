0c2047efe874d130c46028527e677199be07a77d
jasa241 <johansanchez599@gmail.com>
2024-05-08 20:54:06 -0500

    trabjando con env

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,26 @@ import { ServeStaticModule } from "@nestjs/serve-static";
 import { MongooseModule } from "@nestjs/mongoose";
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from "@nestjs/config";
+import { AppsettingsConfiguration } from "./common/config/appsettings.config";
+import { JoiValidationSchema } from "./common/config/joi.validation";
 
 
 @Module({
     imports: [
+
+        ConfigModule.forRoot({
+            load: [AppsettingsConfiguration],
+            validationSchema: JoiValidationSchema
+        }),
+
         ServeStaticModule.forRoot({
             rootPath:join(__dirname,'..','public'),
         }),
 
-        MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+        MongooseModule.forRoot( process.env.MONGODB, {
+            dbName: 'pokemonsDb'
+        } ),
 
         PokemonModule,
 
@@ -22,4 +33,5 @@ import { SeedModule } from './seed/seed.module';
         SeedModule
     ]
 })
-export class AppModule {}
+export class AppModule {
+}
